c462e9e1d50dd867140838ceed081d74b9d1800f
Michael Misch <michealmisch1985@gmail.com>
2023-04-17 21:50:33 -0700

    Organize around using kernel modules for the mount points

---
+++ envfs/envfs.h
@@ -0,0 +1,2 @@
+/* For per-process, this will have to live in the kernel. For now, it'll be a simple flat FUSE fs that wraps calls to get and set env variables for the system */
+
--- srvfs/srvfs.h
+++ srvfs/srvfs.h
@@ -3,5 +3,7 @@
  * Open, write in fd#
  * when a file opens it, dup the original fd out to the new one
  *  - read, write, list only required
+ *  - this could exist in fuse or kernel, start with fuse and see where we're limited
  */
+
 
